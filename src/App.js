6859780e15b2b578066719aed20d26224aee5d7e
Yeomdongsu <117874997+Yeomdongsu@users.noreply.github.com>
2024-02-02 21:29:45 +0900

    수정

--- src/App.js
+++ src/App.js
@@ -1,65 +1,24 @@
 import { useEffect, useRef, useState } from 'react';
 import './App.css';
 import axios from 'axios';
-
-const ChatMessage = ({ sender, message, align }) => {
-  // const [audio, setAudio] = useState(null);
-
-  // useEffect(() => {
-  //   if (audio !== null) {
-  //     console.log(audio);
-  //     audio.play();
-  //   }
-  // }, [audio]);
-
-  function textToSpeech(){
-    axios.post(`${process.env.REACT_APP_URL}/tts`, { content: message })
-      .then((res) => {
-        console.log(res);
-        const audioBlob = new Blob([res.data], { type: "audio/mpeg" });
-        const audioUrl = URL.createObjectURL(audioBlob);
-        
-        const audioElement = new Audio(audioUrl);
-        audioElement.play();
-      })
-      .catch((error) => console.log(error));
-  };
-
-  return (
-    <li className={align === 'right' ? 'right' : 'left'}>
-      {align === "left" ? (
-        <>
-          <div style={{ display: "flex", alignItems: "center" }}>
-            <img style={{ width: "20px", marginLeft: "20px" }} src={process.env.PUBLIC_URL + '/gpt.png'} alt="GPT Logo" />
-            <span className="sender1">{sender}</span>
-            <span className="sender1" style={{ color: "blue", cursor: "pointer" }} onClick={textToSpeech}>음성으로 듣기</span>
-          </div>
-          <div className="message"><span>{message}</span></div>
-        </>
-      ) : (
-        <>
-          <div className="sender"><span>{sender}</span></div>
-          <div className="message"><span>{message}</span></div>
-        </>
-      )}
-    </li>
-  );
-};
+import ChatMessage from './ChatMessage';
 
 const App = () => {
+
   const [inputText, setInputText] = useState("");
   const [chat, setChat] = useState([]);
+  const [loading, setLoading] = useState(false);
 
   const chatRef = useRef(null);
 
   // useEffect를 사용하여 스크롤을 아래로 내림
   useEffect(() => {
     if (chatRef.current) {
-      chatRef.current.scrollTop = chatRef.current.scrollHeight;
+      if (chatRef.current.scrollTop < chatRef.current.scrollHeight) {
+        chatRef.current.scrollTop = chatRef.current.scrollHeight;
+      }
     }
   }, [chat]);
-
-  
 
   const sendMessage = () => {
     if (inputText == "") return window.alert("내용을 입력하세요");
@@ -67,44 +26,59 @@ const App = () => {
     setChat((prevChat) => [...prevChat, { sender: "User", message: inputText }]);
     setInputText("");
 
+    setLoading(true);
+
     axios.post(`${process.env.REACT_APP_URL}/gpt`, {"content": inputText})
     .then((res) => {
+      setLoading(false);
       console.log(res.data.ChatGPT);
       setChat((prevChat) => [...prevChat, { sender: "ChatGPT", message: res.data.ChatGPT }]);
     })
     .catch((e) => console.log(e));
-    
   };
 
   return (
           <div className="container">
             <div className="chat_wrap">
-      <div className="header">CHAT</div>
-      <div className="chat">
-        <ul>
-          {chat.map((message, index) => (
-            <ChatMessage
-              key={index}
-              sender={message.sender}
-              message={message.message}
-              align={message.sender === "User" ? "right" : "left"}
-            />
-          ))}
-        </ul>
-      </div>
-      <div className="input-div">
-        <textarea
-          placeholder="엔터를 누르면 전송됩니다."
-          value={inputText}
-          onChange={(e) => setInputText(e.target.value)}
-          onKeyDown={(e) => {
-            if (e.key === "Enter") sendMessage();
-          }}
-        ></textarea>
-      </div>
-    </div> 
+              <div className="header">Dong GPT</div>
+              <div className="chat" ref={chatRef}>
+                {loading == true ? (
+                  <>
+                    <img src={process.env.PUBLIC_URL + '/loading.gif'} alt="로딩 중" style={{ width: "80px", height: "80px", position: "absolute", top: "50%", left: "50%", transform: "translate(-50%, -50%)" }}/>
+                    <ChatUI chat={chat}/>
+                  </>
+                  ) : (
+                    <ChatUI chat={chat}/>
+                )}
+              </div>
+              <div className="input-div">
+                <textarea
+                  placeholder="엔터를 누르면 전송됩니다."
+                  value={inputText}
+                  onChange={(e) => setInputText(e.target.value)}
+                  onKeyDown={(e) => {
+                    if (e.key === "Enter") sendMessage();
+                  }}
+                ></textarea>
+              </div>
+            </div> 
           </div>
   );
 };
+
+function ChatUI(props){
+  return (
+    <ul>
+      {props.chat.map((message, index) => (
+        <ChatMessage
+          key={index}
+          sender={message.sender}
+          message={message.message}
+          align={message.sender === "User" ? "right" : "left"}
+        />
+      ))}
+    </ul>
+  );
+}
 
 export default App;
---
+++ src/ChatMessage.js
@@ -0,0 +1,98 @@
+import { useState } from "react";
+import axios from "axios";
+
+const ChatMessage = ({ sender, message, align }) => {
+
+    const [audioElement, setAudioElement] = useState(null);
+    const [isPlaying, setIsPlaying] = useState(false);
+    const [loading, setLoading] = useState(false);
+
+    function MantextToSpeech(){
+        setLoading(true);
+
+        axios.post(`${process.env.REACT_APP_URL}/tts`, { content: message, type: "ko-KR-Neural2-C"}, { responseType: 'arraybuffer' })
+        .then((res) => {
+            setLoading(false);
+            resFunc(res.data);
+        })
+        .catch((error) => console.log(error));
+    };
+
+    function WomantextToSpeech(){
+        setLoading(true);
+
+        axios.post(`${process.env.REACT_APP_URL}/tts`, { content: message, type: "ko-KR-Wavenet-A" }, { responseType: 'arraybuffer' })
+        .then((res) => {
+            setLoading(false);
+            resFunc(res.data);
+        })
+        .catch((error) => console.log(error));
+    };
+
+    function resFunc(data){
+        const audioBlob = new Blob([data], { type: "audio/mpeg" });
+        const audioUrl = window.URL.createObjectURL(audioBlob);
+        const audioElement = new Audio(audioUrl);
+        audioElement.play();
+
+        setIsPlaying(true);
+        setAudioElement(audioElement);
+
+        // 오디오가 끝나면 audioElement를 null로 설정
+        audioElement.addEventListener('ended', () => {
+            setAudioElement(null);
+            setIsPlaying(false); 
+        });
+    }
+
+    function pause(){
+        if (audioElement){
+            if (isPlaying){
+                audioElement.pause();
+                setIsPlaying(false);
+            } else {
+                audioElement.play();
+                setIsPlaying(true);
+            }
+        } else {
+            window.alert("재생중이 아닙니다.");
+        }
+    }
+
+    return (
+        <li className={align === 'right' ? 'right' : 'left'}>
+            {align === "left" ? (
+                loading == true ? (
+                    <>
+                        <img src={process.env.PUBLIC_URL + '/speech.gif'} alt="로딩 중" style={{ width:"166px", height:"125px", position: "absolute", top: "50%", left: "50%", transform: "translate(-50%, -50%)" }}/>
+                        <GptUI sender={sender} message={message} MantextToSpeech={MantextToSpeech} WomantextToSpeech={WomantextToSpeech} pause={pause}/>
+                    </>
+                ) : (
+                    <GptUI sender={sender} message={message} MantextToSpeech={MantextToSpeech} WomantextToSpeech={WomantextToSpeech} pause={pause} />
+                )
+            ) : (
+                <>
+                    <div className="sender"><span>{sender}</span></div>
+                    <div className="message"><span>{message}</span></div>
+                </>
+            )}
+        </li>
+    );
+};
+
+function GptUI({ sender, message, MantextToSpeech, WomantextToSpeech, pause }){
+    return (
+        <>
+            <div style={{ display: "flex", alignItems: "center" }}>
+                <img style={{ width: "20px", marginLeft: "20px" }} src={process.env.PUBLIC_URL + '/gpt.png'} alt="GPT Logo" />
+                <span className="sender1">{sender}</span>
+                <span className="sender1" style={{ color: "blue", cursor: "pointer" }} onClick={() => MantextToSpeech()}>남자</span>
+                <span className="sender1" style={{ color: "red", cursor: "pointer" }} onClick={() => WomantextToSpeech()}>여자</span>
+                <img style={{ width: "20px", marginLeft:"3px", paddingTop:"1px" }} src={process.env.PUBLIC_URL + '/stop2.jpg'} alt="Stop Logo" onClick={() => pause()}/>
+            </div>
+            <div className="message"><span>{message}</span></div>
+        </>
+    );
+}
+
+export default ChatMessage;
